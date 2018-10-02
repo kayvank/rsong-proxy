6eea9969e21432c16f7b880da8f541e235f486ac
kayvank <kayvan@q2io.com>
2018-10-02 12:46:09 -0700

    fix new proto changes to rchain

--- scripts/startValidator.sh
+++ scripts/startValidator.sh
@@ -4,6 +4,4 @@
 rnode run -s \
       --required-sigs 0 \
       --map_size 2048576000 \
-      --thread-pool-size 70 \
-      --validator-private-key \
-      b081323f1de029986252e28bd2c5f5dd2309551851d2cc5209b2d1025d6472f4
+      --thread-pool-size 5
